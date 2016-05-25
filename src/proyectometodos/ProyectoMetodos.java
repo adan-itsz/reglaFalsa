a4dc7d80f8501fb707dc373209d8641edd81062f
Adan De Santiago Avalos <adan1995a@gmail.com>
2016-05-25 18:10:24 -0500

    agregado cantidad cifras significativas

--- src/proyectometodos/ProyectoMetodos.java
+++ src/proyectometodos/ProyectoMetodos.java
@@ -8,6 +8,7 @@ package proyectometodos;
 import java.io.File;
 import java.io.FileWriter;
 import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class ProyectoMetodos {
    // double c= 18.00;//coeficiente de rozamiento
@@ -28,9 +29,10 @@ public class ProyectoMetodos {
     int suma=0;
     int itera=0;
     int numIteraciones;
+    String cifrasString;
     File archivo;
     FileWriter escribir;
-    DecimalFormat decimales = new DecimalFormat("0.0000");
+    DecimalFormat decimales = new DecimalFormat("0.0000000000");
     public ProyectoMetodos(double a, double b,int numIteraciones) {
         reglaFalsa(a,b,numIteraciones);
     }
@@ -44,16 +46,16 @@ public class ProyectoMetodos {
     boolean bandera= false;
     
     
-    iteraccion= "iteraccion" + "\t" + "X+" + "\t\t" + "X-" + "\t\t\t" + "X" +"\t\t"+ "f(x)"+ "\t\t\t\t" +"ER" + "\n" ;
+    iteraccion= "\n iteraccion" + "\t" + "X+" + "\t\t\t" + "X-" + "\t\t\t" + "X" +"\t\t\t"+ "f(x)"+ "\t\t\t\t" +"ER" + "\n" ;
     guardarArchivo(iteraccion);
-    while(itera<numIteraciones){//suma=0
+    while(itera<numIteraciones){
     fDea= ((Math.pow(a,3))-a-3);//sustitucion de funcion con valor negativo
     fDeb=(Math.pow(b,3))-b-3;//sustitucion de funcion  con valor positivo
   
     resultado=((b*fDea)-(a *fDeb))/(fDea-fDeb);// igual a C
     fDeResultado=(Math.pow(resultado, 3)-resultado - 3) ;//
     if(itera==0){
-    iteraccion= " \t"+  itera + " \t" + decimales.format(b)  + "\t\t" + decimales.format(a) +"\t\t\t"+ decimales.format(resultado) +"\t\t" + decimales.format(fDeResultado) +"\t\t" + "\t\t"+decimales.format(relativo)+"\n";
+    iteraccion= " \t"+  itera + " \t" + decimales.format(b)  + "\t\t" + decimales.format(a) +"\t\t"+ decimales.format(resultado) +"\t\t" + decimales.format(fDeResultado) +"\t\t\t"+decimales.format(relativo)+"\n";
     guardarArchivo(iteraccion);
    // itera++;
     if(!esPositivo(fDeResultado)){
@@ -77,8 +79,9 @@ public class ProyectoMetodos {
         }
     }
         relativo=errorRelativo(resultado,cAnterior);
+        
     if(bandera){
-     iteraccion= " \t"+  itera + " \t" + decimales.format(b)  + "\t\t" + decimales.format(cAnterior) +"\t\t\t"+ decimales.format(resultado) +"\t\t" + decimales.format(fDeResultado) +"\t\t" + "\t\t"+decimales.format(relativo)+"\n";
+     iteraccion= " \t"+  itera + " \t" + decimales.format(b)  + "\t\t" + decimales.format(cAnterior) +"\t\t"+ decimales.format(resultado) +"\t\t" + decimales.format(fDeResultado) +"\t\t\t"+decimales.format(relativo)+"\n";
     guardarArchivo(iteraccion);
     
     
@@ -89,7 +92,9 @@ public class ProyectoMetodos {
     
     }
     sumaL=numCifrasSignificativas(relativo);
-   
+    cifrasString="numero de cifras significativas: "+ sumaL +"\n";
+    
+   guardarArchivo(cifrasString);
     
     
     }
@@ -127,10 +132,11 @@ public class ProyectoMetodos {
     
     public int numCifrasSignificativas(double error){
         String cifrasSig="";
+        suma=0;
         double errorLocal=error;
         char mayorCuatro;
         boolean ban= false;
-        cifrasSig= Double.toString(errorLocal);
+        cifrasSig= (decimales.format(errorLocal)) + '4';
         char[]cifraArray= cifrasSig.toCharArray();
         for (int i = 0; i < cifrasSig.length() ; i++) {
             if(!ban){
@@ -142,15 +148,18 @@ public class ProyectoMetodos {
             else{
             
             if(cifraArray[i]=='0'){
-                if(cifraArray[i +1]<5){
+                if(cifraArray[i +1]=='0'||cifraArray[i +1]=='1'||cifraArray[i +1]=='2'||cifraArray[i +1]== '3'||cifraArray[i +1]== '4'){
                 suma++;
+                }
+                else{
+                break;
                 }
             
             }
             else{
         
                 
-            break;
+         //   break;
             }
             }
             
@@ -172,8 +181,30 @@ public class ProyectoMetodos {
     
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner scan= new Scanner(System.in);
+        int opcion;
+        int tipoError;
+        int cantidadIteracciones=0;
         
-      ProyectoMetodos s=  new ProyectoMetodos(1,2,10);
+        
+        System.out.println("Terminar metodo por:");
+        System.out.println("1- numero de iteracciones");
+        System.out.println("2- terminar por error:");
+        opcion= scan.nextInt();
+        switch(opcion){
+            case 1:
+                System.out.println("Cantidad de iteracciones");
+                cantidadIteracciones= scan.nextInt();
+                break;
+            case 2: cantidadIteracciones=-1;   
+            break;
+        }
+         System.out.println("Elige el tipo de error");
+        System.out.println("1- Error relativo:");
+        System.out.println("2- Error absoluto");
+        tipoError=scan.nextInt();
+        
+      ProyectoMetodos s=  new ProyectoMetodos(1,2,cantidadIteracciones);
     }
    
     
